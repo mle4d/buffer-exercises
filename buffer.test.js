c2bee571790a964f983c624f97bbded3d9e2564f
emily <mle4d@me.com>
2019-07-09 11:12:20 -0700

    buff

--- buffer.test.js
+++ buffer.test.js
@@ -1,7 +1,19 @@
 describe('buffers', () => {
-  it('creates a buffer of length 10', () => {
+  it('buffer to 10', () => {
     const buf = Buffer.alloc(10);
     expect(buf).toHaveLength(10);
+  });
+});
+
+describe('buffering', () => {
+  it('buffasfuck', () => {
+    const buf = Buffer.from('oh hai');
+
+    expect(buf).toHaveLength(6);
+    expect(buf.toString()).toEqual('oh hai');
+
+    const buffy = buf.map(() => 104);
+    expect(buffy.toString()).toEqual('hhhhhh');
   });
 });
 
---
+++ index.js
@@ -0,0 +1,4 @@
+const buffer =  Buffer.from('hello');
+
+Buffer.alloc()
+
